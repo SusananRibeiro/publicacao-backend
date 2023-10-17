5c99da719e648ceb3e0ffa65ab3d92ff2d429005
Susana_Ribeiro <susanadonribeiro@gmail.com>
2023-10-17 20:47:54 -0300

    Atualizando a branch master

--- src/main/java/com/fabricio/resource/ProdutoResource.java
+++ src/main/java/com/fabricio/resource/ProdutoResource.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProdutoResource {
 
-    @Inject
+    @Inject // para injeção de dependência
     private ProdutoRepository repository;
 
     @GET
